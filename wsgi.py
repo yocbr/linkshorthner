c85fd4eed1a23a41fd1292c41b1b0d28af31268a
caribulov <92824505+caribulov@users.noreply.github.com>
2022-10-31 23:49:45 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,15 @@
 """
 WSGI config for shorty project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+
 
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
+It exposes the WSGI callable as a module-level variable named ``application``.
 
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
