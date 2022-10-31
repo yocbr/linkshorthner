0bfb5e007ba1b808a41a4e5e591820edaab9743d
caribulov <92824505+caribulov@users.noreply.github.com>
2022-10-31 23:51:27 +0530

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,7 +1,10 @@
 import graphene
 
 import shortener.schema
+It exposes the WSGI callable as a module-level variable named ``application``.
 
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 class Query(shortener.schema.Query, graphene.ObjectType):
     pass
